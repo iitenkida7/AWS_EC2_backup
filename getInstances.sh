1725f83be2554c4e9d2c2914aae37c7cf5abc8d6
TakeshiIshii <takeshi.ishii@mfro.net>
2016-03-14 16:36:44 +0900

    only ebs mount servers

--- getInstances.sh
+++ getInstances.sh
@@ -2,7 +2,7 @@
 
 AWS_PROFILE=$1
 
-aws ec2 describe-instances --profile ${AWS_PROFILE} --out json   \
+aws ec2 describe-instances --profile ${AWS_PROFILE} --filters "Name=block-device-mapping.status,Values=attached" --out json   \
 | jq -r   ".Reservations[].Instances[] | {InstanceId, InstanceName: (.Tags[] | select(.Key==\"Name\").Value)}"  \
 | jq -r --slurp   '.[] | .InstanceId + "\t" +  .InstanceName'
 
